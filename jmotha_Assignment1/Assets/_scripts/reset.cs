7ba490af4e243e25b1ddabca599b1aed1c9af0dc
Joseph Motha <josephmotha@outlook.com>
2019-01-28 16:25:58 -0500

    update reset

--- jmotha_Assignment1/Assets/_scripts/reset.cs
+++ jmotha_Assignment1/Assets/_scripts/reset.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class reset : MonoBehaviour
 {
     // varibale to store start position of object
-    Vector3 startposition;
-    Vector3 originalrotation;
-
+    private Vector3 _startposition;
+    private Vector3 _originalrotation;
+    private Rigidbody _myrigidbody;
     // Start is called before the first frame update
     void Start()
     {
         // stores start position
-        startposition = transform.position;
-        originalrotation = transform.eulerAngles;
+        _myrigidbody = GetComponent<Rigidbody>();
+        _startposition = transform.position;
+        _originalrotation = transform.eulerAngles;
+
+        
 
     }
 
@@ -29,8 +32,10 @@ public class reset : MonoBehaviour
         if (Input.GetMouseButtonUp(1))
         {
             //sets position to original start position
-            transform.position = startposition;
-            transform.eulerAngles = originalrotation;
+            transform.position = _startposition;
+            transform.eulerAngles = _originalrotation;
+            _myrigidbody.velocity = Vector3.zero;
+            _myrigidbody.angularVelocity = Vector3.zero;
 
         }
     }
