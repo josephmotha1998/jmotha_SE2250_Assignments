1f7053ca1b752411535454e4911604d2b686b378
Joseph Motha <josephmotha@outlook.com>
2019-02-11 17:08:12 -0500

    addedd assignment2 to repo

---
+++ jmotha_SE2250_assignment2/Assets/Scripts/Playermovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Playermovement : MonoBehaviour
+{
+  
+    private Rigidbody _rb;
+    private int _score;
+
+    public float speed;
+    public Text scoretext;
+
+
+    public int Score
+    {
+        get
+        {
+            return _score;
+        }
+
+        set
+        {
+            _score = _score +value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        Score = 0;
+        SetScoreText();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        _rb.AddForce(movement*speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item") && other.gameObject.GetComponent<Renderer>().material.color==Color.blue)
+        {
+            other.gameObject.SetActive(false);
+            Score = 1;
+            SetScoreText();
+
+        }
+        else if (other.gameObject.CompareTag("Item") && other.gameObject.GetComponent<Renderer>().material.color == Color.red)
+        {
+            other.gameObject.SetActive(false);
+            Score =  2;
+            SetScoreText();
+        }
+        else if (other.gameObject.CompareTag("Item") && other.gameObject.GetComponent<Renderer>().material.color == Color.magenta)
+        {
+            other.gameObject.SetActive(false);
+            Score = 3;
+            SetScoreText();
+
+        }
+    } 
+
+    void SetScoreText()
+    {
+        scoretext.text = "Score: " + Score.ToString();
+    }
+
+}
---
+++ jmotha_SE2250_assignment2/Assets/Scripts/prefabscript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefabscript : MonoBehaviour
+{
+    public GameObject box;
+    private GameObject _gameobject;
+    private List<GameObject> _golist;
+    private Color[] _myarray;
+
+    public float numofGO = 9;
+    public float length = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myarray = new Color[3];
+        _golist = new List<GameObject>();
+        _myarray[0]=(Color.blue);
+        _myarray[1]=(Color.red);
+        _myarray[2]=(Color.magenta);
+
+
+        for (int i = 0; i < numofGO; i++)
+        {
+            float angle = i * Mathf.PI * (2 / numofGO);
+            Vector3 pos = new Vector3(Mathf.Cos(angle), 0.1f, Mathf.Sin(angle)) * length;
+            _gameobject = Instantiate<GameObject>(box, pos, Quaternion.identity);
+            _gameobject.GetComponent<Renderer>().material.color = _myarray[i%3];
+            _golist.Add(_gameobject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
