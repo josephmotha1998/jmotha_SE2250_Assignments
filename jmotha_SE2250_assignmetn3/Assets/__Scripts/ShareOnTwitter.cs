8171e20157ebea5edbb94c063085d43b0874ca93
Joseph Motha <josephmotha@outlook.com>
2019-03-18 14:54:38 -0400

    made changes to errors in code

---
+++ jmotha_SE2250_assignmetn3/Assets/__Scripts/ShareOnTwitter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShareOnTwitter : MonoBehaviour
+{
+    string twitter_address = "https://twitter.com/intent/tweet";
+
+    string tweet_language = "en";
+
+    string displaytext = "Check out my new score: ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Share()
+    {
+        Application.OpenURL(twitter_address + "?text=" + WWW.EscapeURL(displaytext) + ScoreManager.HIGH_SCORE + "&amp;lang=" + WWW.EscapeURL(tweet_language));
+    }
+}
